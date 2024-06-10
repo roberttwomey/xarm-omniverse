d8730db70422d660d809abd2f50895563a0c3cdc
Robert Twomey <robert.twomey@gmail.com>
2024-06-10 12:23:54 -0500

    added a few more lite6 scripts

---
+++ run_lite6_isaac.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "starting isaac sim"
+cd /home/user/work/xarm-omniverse/scripts/XArm
+/home/user/.local/share/ov/pkg/isaac_sim-2022.2.1/python.sh lite6_follow_target_with_standalone.py
